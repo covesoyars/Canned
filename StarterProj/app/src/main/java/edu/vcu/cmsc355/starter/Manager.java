7964da7792a9584c43c9fa85975baed0ed6f9154
Cove Soyars <soyarsc@vcu.edu>
2019-02-26 16:46:38 -0500

    added version 0.1 of Manager class

---
+++ StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/Manager.java
@@ -0,0 +1,26 @@
+/**
+ * @version 0.1 of Manager class
+ * Has method to verify a volunteer object
+ * Needs:
+ *  method to add storage locations (depends on food database)
+ *  method to remove volunteers (depends on volunteer database)
+ *
+ */
+
+package edu.vcu.cmsc355.starter;
+import java.io.File;
+
+public class Manager extends User {
+
+
+    public Manager(String aPassword, String aUserName, String aFirstName, String aLastName, int aDob,
+                     String anEmail, File aPicture){
+
+        super(aPassword, aUserName, aFirstName, aLastName,aDob,anEmail, aPicture);
+
+    }
+
+    public void verifyVolunteer(Volunteer aVol){
+        aVol.verify();
+    }
+}
