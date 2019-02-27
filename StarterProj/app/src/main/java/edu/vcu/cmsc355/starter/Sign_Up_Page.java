572a3c31a6814fed72395cbe78bbc2fd5b982ab6
Justin Nelson <JDNelson109@gmail.com>
2019-02-26 20:57:11 -0500

    Added sign up page and connected sign up button

---
+++ StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/Sign_Up_Page.java
@@ -0,0 +1,13 @@
+package edu.vcu.cmsc355.starter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sign_Up_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign__up__page);
+    }
+}
