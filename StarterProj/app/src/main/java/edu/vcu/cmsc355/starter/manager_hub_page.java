bbb94fa845afa5dfdba20adbe6f614fea262909e
Xavi Moreira <moreiraje@vcu.edu>
2019-03-09 20:51:34 -0500

    manager hub update

---
+++ StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/manager_hub_page.java
@@ -0,0 +1,16 @@
+package edu.vcu.cmsc355.starter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class manager_hub_page extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.manager_hub_page);
+
+        //configureBackButton();
+    }
+}
