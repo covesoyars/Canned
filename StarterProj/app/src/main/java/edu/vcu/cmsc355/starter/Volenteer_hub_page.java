e6467187973d296b162306f23d6f5be9532f4223
Xavi Moreira <moreiraje@vcu.edu>
2019-03-10 19:54:17 -0400

    added volenteer page

---
+++ StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/Volenteer_hub_page.java
@@ -0,0 +1,13 @@
+package edu.vcu.cmsc355.starter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Volenteer_hub_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volenteer_hub_page);
+    }
+}
