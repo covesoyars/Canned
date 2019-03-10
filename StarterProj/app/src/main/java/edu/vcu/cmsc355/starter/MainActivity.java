7df2c9564e5e34ffef8447cd7a91346a7e27ca1f
Justin Nelson <JDNelson109@gmail.com>
2019-03-10 16:20:09 -0400

    Manager login should work.

--- StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/MainActivity.java
+++ StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/MainActivity.java
@@ -4,13 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText user;
+    private EditText pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user = (EditText) findViewById(R.id.editText);
+        pass = (EditText) findViewById(R.id.editText2);
     }
 
     public void signUp(View view){
@@ -21,5 +26,14 @@ public class MainActivity extends AppCompatActivity {
     public void forgotInfo(View view){
         Intent startforgotInfo = new Intent(this, Forgot_info.class);
         startActivity(startforgotInfo);
+    }
+
+    public void login(View view){
+        Intent login = new Intent(this, manager_hub_page.class);
+        //Intent volLogin = new Intent(this, volunteer_hub_page.class);
+
+        if(user.equals("CoolKid123") && pass.equals("CoolPass")) {
+            startActivity(login);
+        }
     }
 }
