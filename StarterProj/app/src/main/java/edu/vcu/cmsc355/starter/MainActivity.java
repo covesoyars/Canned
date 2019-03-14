86af5e850938195a829033ebcbc82507672924e2
Cove Soyars <soyarsc@vcu.edu>
2019-03-14 14:25:32 -0400

    added file to save logged in user's information

--- StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/MainActivity.java
+++ StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/MainActivity.java
@@ -37,6 +37,16 @@ public class MainActivity extends AppCompatActivity {
         Intent login = new Intent(this, manager_hub_page.class);
         //Intent volLogin = new Intent(this, volunteer_hub_page.class);
         if(user.getText().toString().equals("CoolKid123") && pass.getText().toString().equals("CoolPass")) {
+
+            User loggedInUser = new Volunteer("CoolPass", // create user object (will pull
+                    // user with matching pw and username when database is working
+                    "CoolKid123",
+                    "Doug",
+                    "Dimmadome",
+                    123456,
+                    "doug.dimmadome@dimmsdaledimmadome.com");
+            UserLoggedIn appState = ((UserLoggedIn) getApplicationContext());
+            appState.setLoggedIn(loggedInUser);
             startActivity(login);
         }
         else{
