a66f62c507e0273448c3189f36247d2da5a0bc57
Sam Portillo <portillosc@mymail.vcu.edu>
2019-03-26 11:59:28 -0400

    created the add food item page; need to connect it with firebase

--- StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/Volenteer_hub_page.java
+++ StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/Volenteer_hub_page.java
@@ -1,7 +1,9 @@
 package edu.vcu.cmsc355.starter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Volenteer_hub_page extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Volenteer_hub_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volenteer_hub_page);
+    }
+
+    public void recordDonations(View View)
+    {
+        Intent rec = new Intent(this, recordDonations.class);
+        startActivity(rec);
     }
 }
---
+++ StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/recordDonations.java
@@ -0,0 +1,20 @@
+package edu.vcu.cmsc355.starter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class recordDonations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_donations);
+    }
+
+    //ADD A RESET BUTTON
+
+    public void record(View view){
+
+    }
+}
