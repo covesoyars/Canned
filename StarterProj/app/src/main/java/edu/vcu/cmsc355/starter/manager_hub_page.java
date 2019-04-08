a4063eee1370b8ee83d0f5f85d4868e553f39d60
Cove Soyars <soyarsc@vcu.edu>
2019-04-08 13:19:05 -0400

    Added file picker for csv (will need to use it to update inventory soon)

--- StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/manager_hub_page.java
+++ StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/manager_hub_page.java
@@ -1,9 +1,18 @@
 package edu.vcu.cmsc355.starter;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+
+import java.util.regex.Pattern;
 
 public class manager_hub_page extends AppCompatActivity {
 
@@ -31,8 +40,17 @@ public class manager_hub_page extends AppCompatActivity {
         startActivity(mv);
     }
     public void addCSV(View view){
-        Intent csvPage = new Intent(this, CSV_Page.class);
-        startActivity(csvPage);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 10001);
+        }
+        // create file picker object:
+        new MaterialFilePicker()
+                .withActivity(this)
+                .withRequestCode(1)
+                .withHiddenFiles(true) // Show hidden files and folders
+                .start();
     }
 
     public void manageInventory(View view){
@@ -44,4 +62,15 @@ public class manager_hub_page extends AppCompatActivity {
     {
        //TURN THIS INTO A LOGOUT METHOD THINGY -Javier
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+            Toast.makeText(this,filePath,Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
