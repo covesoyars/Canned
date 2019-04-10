f13e6bb1855139c248c0d1eb3450b435cc89a87e
Cove Soyars <soyarsc@vcu.edu>
2019-04-10 15:54:29 -0400

    Forgot info is connected to the db

--- StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/Forgot_info.java
+++ StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/Forgot_info.java
@@ -5,12 +5,22 @@ package edu.vcu.cmsc355.starter;
  */
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.regex.Pattern;
 
@@ -19,6 +29,9 @@ public class Forgot_info extends AppCompatActivity {
     private CheckBox user;
     private CheckBox pass;
     private Volunteer testUser;
+    private User foundHim;
+    private static final String KEY_PASS = "pass";
+
     private static final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile( // regex pattern to validate email addresses
             "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
                     "\\@" +
@@ -38,6 +51,9 @@ public class Forgot_info extends AppCompatActivity {
         testUser = new Volunteer(); // test user
 
 
+
+
+
     }
 
 
@@ -51,8 +67,8 @@ public class Forgot_info extends AppCompatActivity {
         finish();
     }
 
-    public void sendEmail(View view){
-        String address = email.getText().toString();
+    public void sendEmail(View v){
+
         String subject = subject = "Forgot Canned information?"; // set subject
 
         if(email.getText().toString().length() == 0){ // if user didn't enter an email,
@@ -70,38 +86,124 @@ public class Forgot_info extends AppCompatActivity {
             return; // tell user they didn't select anything and end method call
         }
 
-        if(user.isChecked() && pass.isChecked()){ // if both boxes are checked,
-            testUser.forgotPassword(); // reset password
-            // create message with new pw and username:
-            String forgotPassMessage = "Hello,\n\nYour Canned information is:\n" + testUser.getUserName() +
-                   "\n" + testUser.getPassword()  + "\n\nThanks,\nThe Canned Team";
+        findUser(); // search db for user
+        if(foundHim == null){ // if user is not found
+            Toast.makeText(Forgot_info.this, "No user with that email found.", Toast.LENGTH_SHORT).show();
 
-            EmailSender forgotPass = new EmailSender(forgotPassMessage, address, "postmaster@automail-canned.com", subject);
+            return; // break
+        }
+
+        if(user.isChecked() && pass.isChecked()){ // if both boxes are checked,
+            foundHim.forgotPassword(); // reset password
+            updatePW();
+            // create message with new pw and username:
+            String forgotPassMessage = "Hello,\n\nYour Canned information is:\n" + foundHim.getUserName() +
+                   "\n" + foundHim.getPassword()  + "\n\nThanks,\nThe Canned Team";
+
+            EmailSender forgotPass = new EmailSender(forgotPassMessage, foundHim.getEmailAddress(), "postmaster@automail-canned.com", subject);
             forgotPass.send();
 
         }
         else if(user.isChecked()){  // if just username box is checked,
             // create message with username:
-            String forgotPassMessage = "Hello,\n\nYour Canned username is:\n" + testUser.getUserName() +
+            String forgotPassMessage = "Hello,\n\nYour Canned username is:\n" + foundHim.getUserName() +
                     "\n\nThanks,\nThe Canned Team";
 
-            EmailSender forgotPass = new EmailSender(forgotPassMessage, address, "postmaster@automail-canned.com", subject);
+            EmailSender forgotPass = new EmailSender(forgotPassMessage, foundHim.getEmailAddress(), "postmaster@automail-canned.com", subject);
             forgotPass.send();
 
         }
         else if(pass.isChecked()){ // if just password box is checked,
-            testUser.forgotPassword(); // reset user's password
+            foundHim.forgotPassword(); // reset user's password
+            updatePW();
             // create message with new password
-            String forgotPassMessage = "Hello,\n\nYour new Canned password is:\n" + testUser.getPassword() +
+            String forgotPassMessage = "Hello,\n\nYour new Canned password is:\n" + foundHim.getPassword() +
                     "\n\nThanks,\nThe Canned Team";
 
-            EmailSender forgotPass = new EmailSender(forgotPassMessage, address, "postmaster@automail-canned.com", subject);
+            EmailSender forgotPass = new EmailSender(forgotPassMessage, foundHim.getEmailAddress(), "postmaster@automail-canned.com", subject);
             forgotPass.send();
         }
+
 
 
         //Take them back to the login page
         Intent mainActivity = new Intent(this, MainActivity.class);
         startActivity(mainActivity);
     }
+
+    public void findUser(){
+        String emailAddress = email.getText().toString();
+
+        // initiaize fb:
+        FirebaseApp.initializeApp(this);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference users = db.collection("users2");
+
+        users.whereEqualTo("email", emailAddress).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                // if it didn't crash connecting to firebase
+                if (task.isSuccessful()) {
+
+                    // result of the search
+                    QuerySnapshot q = task.getResult();
+
+                    // if the result of the search is not empty
+                    if (!q.isEmpty()) {
+
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String p = document.getData().get("pass").toString();
+                            String u = document.getData().get("user").toString();
+                            String f = document.getData().get("first").toString();
+                            String l = document.getData().get("last").toString();
+                            int dob = Integer.parseInt(document.getData().get("date").toString());
+                            String e = document.getData().get("email").toString();
+
+                            foundHim =  new User(p,u,f,l,dob,e);
+                        }
+                    } else {
+
+                    }
+                } else {
+                }
+
+            }
+        });
+
+        }
+
+        public void updatePW(){
+            // updates user's password in db
+            FirebaseApp.initializeApp(this);
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            CollectionReference users = db.collection("users2");
+
+            users.whereEqualTo("user",foundHim.getUserName()).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    // if it didn't crash connecting to firebase
+                    if (task.isSuccessful()) {
+
+                        // result of the search
+                        QuerySnapshot q = task.getResult();
+
+                        // if the result of the search is not empty
+                        if (!q.isEmpty()) {
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                DocumentReference myRef = document.getReference();
+                                myRef.update(KEY_PASS, foundHim.getPassword());
+
+                            }
+                        } else {
+
+                        }
+                    } else {
+
+                    }
+
+                }
+            });
+
+        }
 }
