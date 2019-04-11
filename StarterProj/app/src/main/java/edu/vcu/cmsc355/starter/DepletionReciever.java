cd8a45819f8cbdd40a2c328c2f52335f5904df89
Cove Soyars <soyarsc@vcu.edu>
2019-04-11 12:54:13 -0400

    commented out stuff on addFood

---
+++ StarterProj/app/src/main/java/edu/vcu/cmsc355/starter/DepletionReciever.java
@@ -0,0 +1,14 @@
+package edu.vcu.cmsc355.starter;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+// calculates depletion rate every day at midnight
+public class DepletionReciever extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+
+    }
+}
